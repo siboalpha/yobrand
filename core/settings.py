78fb06fee55b65b5089b84ce41f00066c1be982f
siboalpha <sibo.alphonsee@gmail.com>
2022-04-23 23:05:35 +0200

    Debugging

--- core/settings.py
+++ core/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
+#import environ
 import os
 
 #initialize environment rariables
 
-env = environ.Env()
-environ.Env.read_env()
+#env = environ.Env()
+#environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,10 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-6bk_!q!h-g0fc2bpk(!kuvfmep3-gh=g1!efj@9rpxm#v)5&*9'
+#env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
+#env('DEBUG')
 ALLOWED_HOSTS = ['siboalpha.pythonanywhere.com','www.siboalpha.pythonanywhere.com','localhost']
 
 
